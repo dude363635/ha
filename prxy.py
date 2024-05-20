c2653becfd28148beecb28717b7980d3ed015363
dude363635 <143981761+dude363635@users.noreply.github.com>
2024-05-20 16:20:12 +1000

    Update prxy.py

--- prxy.py
+++ prxy.py
@@ -1,14 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response
 import requests
 
 app = Flask(__name__)
 
-@app.route('/proxy', methods=['GET', 'POST'])
-def proxy():
-    url = 'https://shellshock.io/' + request.full_path
-    headers = {'User-Agent': request.headers.get('User-Agent')}
-    response = requests.request(method=request.method, url=url, headers=headers, data=request.get_data(), cookies=request.cookies, allow_redirects=False)
-    return (response.content, response.status_code, response.headers.items())
+TARGET_URL = 'https://shellshock.io/'
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path):
+    url = f"{TARGET_URL}{path}"
+    headers = {key: value for key, value in request.headers if key != 'Host'}
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers=headers,
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False
+    )
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for name, value in response.raw.headers.items() if name.lower() not in excluded_headers]
+    
+    return Response(response.content, response.status_code, headers)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
